53d17c15ecf347f7e80bcb6ec1bd12516e1ff6ef
D053555 <paolo.sfilio@sap.com>
2022-02-11 18:17:46 +0100

    fix async issues by changing to native interaction methods on Webdriver-level; added some comments as hints

--- pages/home.js
+++ pages/home.js
@@ -11,7 +11,14 @@ module.exports = {
                 viewName: 'sap.ui.demo.cart.view.Home',
             }
         };
-        await browser.asControl(idSelector).setValue('Watch');
+        const searchFieldElement = await browser.asControl(idSelector);
+
+        // Working with UI5 events can be tricky, e.g. the following line does not fire a 'search' event
+        // await browser.asControl(idSelector).setValue('Watch');
+        // But using native interactions (click/setValue) on Webdriver-level seem to work smoothly.
+        // Unfortunately it seems that native interactions cannot be written fully in fluent API style:
+        const webElement = await searchFieldElement.getWebElement();
+        await webElement.$('input[type=search]').setValue('Watch');
     },
 
     async iSelectTheFirstProduct() {
@@ -23,7 +30,14 @@ module.exports = {
                 }
             }
         };
-        await browser.asControl(textSelector).click();
+
+        // Working with UI5 events can be tricky, e.g. the following line does not fire a 'click' event
+        // await browser.asControl(textSelector).click();
+        // But using native interactions (click/setValue) on Webdriver-level seem to work smoothly.
+        // Unfortunately it seems that native interactions cannot be written fully in fluent API style:
+        const textElement = await browser.asControl(textSelector);
+        const webElement = await textElement.getWebElement();
+        await webElement.click();
     },
 
     // Assertions
@@ -38,12 +52,15 @@ module.exports = {
     },
 
     async theProductListShouldBeFiltered() {
+        // The property 'timeout' is important, as the product list needs some milliseconds to update
+        // Without setting a 'maximum waiting time (ms) before failing the search',
+        // the element might not be available in the DOM when accessing it
         const idSelector = {
+            timeout: 1000,
             selector: {
-                id: 'container-cart---homeView--categoryList'
+                id: 'container-cart---homeView--productList'
             }
         };
-
         const title = await browser.asControl(idSelector)?.getItems()[0]?.getTitle();
 
         expect(title).toBe('Flat Watch HD32');
